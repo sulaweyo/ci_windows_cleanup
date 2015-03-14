dd82bf8184a8fe8d4dfbb53e5bf586ca08231c35
Sledge Sulaweyo <sledge.sulaweyo@gmail.com>
2015-03-14 17:15:00 +0100

    Change %TEMP% approach to use a defined dir Switch depending on kernel version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,7 @@
 # Class: ci_windows_cleanup
-#   Parameter: user - defaults to Administrator
+#   Parameter: user - defaults to Administrator, used to define the directory we want to delete
 #
 class ci_windows_cleanup ($user = 'Administrator') {
-  $temp_path = '%TEMP%'
 
   # in my environment around noon is kind of low load so let's schedule it here
   schedule { 'ci_windows_cleanup':
@@ -12,11 +11,16 @@ class ci_windows_cleanup ($user = 'Administrator') {
   }
 
   if $::kernel == 'windows' {
+    # Define the path to the temp directory
+    if $::kernelmajorversion >= '6.0' {
+      $temp_path = "C:\\Users\\${user}\\AppData\\Local\\Temp"
+    } else {
+      $temp_path = "C:\\Documents and Settings\\${user}\\Local Settings\\Temp"
+    }
     # Purge the %TEMP% directory completely. Some files might be locked as they are used so we accept more exit codes.
     exec { 'windows_cleanup_purge_temp':
       command   => "cmd /c rmdir /s/q ${temp_path}",
       path      => $::path,
-      user      => $user, # the temp location is user specific so make sure to run this class for the correct user
       cwd       => "${temp_path}\\..\\", # change into TEMPs parent folder to run the cleanup command
       logoutput => false, # we delete quiet so nothing interesting here in the first place
       timeout   => 900, # 15 minutes are enough, if not we come back here tomorrow anyway
@@ -29,7 +33,6 @@ class ci_windows_cleanup ($user = 'Administrator') {
     exec { 'windows_cleanup_recreate_temp':
       command     => "cmd /c mkdir ${temp_path}",
       path        => $::path,
-      user        => $user, # the temp location is user specific so make sure to run this class for the correct user
       returns     => [0, 1], # if the dir already exists we are fine too
       refreshonly => true,
     }
@@ -38,7 +41,6 @@ class ci_windows_cleanup ($user = 'Administrator') {
     exec { 'windows_cleanup_purge_recycle_bin':
       command   => "cmd /c rmdir /s/q %systemdrive%\\\$Recycle.bin",
       path      => $::path,
-      user      => 'Administrator', # here we need to be Administrator
       logoutput => false, # we delete quiet so nothing interesting here in the first place
       timeout   => 900, # 15 minutes are enough, if not we come back here tomorrow anyway
       returns   => [0, 5, 32], # 0 success, 5 access denied, 32 sharing violation -> all fine as everything not in use got deleted
